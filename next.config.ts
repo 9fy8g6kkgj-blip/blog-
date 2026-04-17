955eaa5211723013f10c9770bfd43c61ae71fdef
alstjd <alstjd0263@naver.com>
2026-04-17 13:20:02 +0900

    fix: remove duplicate declaration

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! 핵심: 타입 에러가 있어도 빌드를 강제로 완료합니다.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // ESLint 에러도 무시합니다.
     ignoreDuringBuilds: true,
   },
 };
